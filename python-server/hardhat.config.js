bb678e58478ab10fef4f55daa9fbcf16035f4de0
Alessandro Pavesi <pavesialessandro@live.it>
2025-02-10 23:13:23 +0100

    fix network

--- python-server/hardhat.config.js
+++ python-server/hardhat.config.js
@@ -4,7 +4,7 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.28",
   networks: {
-    ganache: {
+    backend: {
       url: "http://ganache:8545",
       accounts: ["0x0cb722dc6d52099482594ca293e5b2c19ef4934d05a610ffcdf05b33cb1ac0cc"]
     }
